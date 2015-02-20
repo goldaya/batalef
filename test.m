99acaa410471cdc96fc2a686bc35a5a032bc5cec
uido123 <uido123@gmail.com>
2015-02-20 09:11:10 +0200

    gitplay testings

--- test.m
+++ test.m
@@ -1,11 +1,3 @@
 function [ point1, point2 ] = test( hObj, evt )
 % THIS IS KIND OF MAGIC !
-%TEST Summary of this function goes here
-%   Detailed explanation goes here
-
-        point1 = get(hObj,'CurrentPoint');
-        rbbox;
-        point2 = get(hObj,'CurrentPoint');
-        
 end
-
