daefb19add1f90f2e66077cb5459c89d8f1f41f9
uido123 <uido123@gmail.com>
2015-06-25 10:05:33 +0300

    v0.4.2 buffer in secondary files creation

--- code/channelCalls/createSecondaryTS.m
+++ code/channelCalls/createSecondaryTS.m
@@ -1,14 +1,12 @@
-function [ newTS ] = createSecondaryTS( k )
+function [ newTS ] = createSecondaryTS( k,buffer )
 %CREATESECONDARYTS -INTERNAL- flattens the channel calls in a TS.
 
     % get full TS
     N  = fileData(k,'Channels','Count');
-    %TS = fileData(k,'TS');
     Fs = fileData(k,'Fs');
-    buffer = 0.5*getParam('secondaryFiles:buffer')/1000;
     newTS = zeros(fileData(k,'nSamples'),N);
     
-    
+    % create new TS
     for j = 1:N
         TS = channelData(k,j,'TS');
         starts = round((cell2mat(channelData(k,j,'Calls','ForLocalization','Times','Start'))-buffer).*Fs);
--- code/filesHandling/createSecondaryFile.m
+++ code/filesHandling/createSecondaryFile.m
@@ -1,6 +1,19 @@
 function createSecondaryFile( K )
 %CREATESECONDARYFILE -INTERNAL- create a secondary file and load into app
 
+    % ask for buffer 
+    bufferParam = getParam('secondaryFiles:buffer');
+    A = inputdlg('Buffer size around each call (msec)','Creating Secondary Files',[1,50],{num2str(bufferParam)});
+    if isempty(A)
+        return;
+    end
+    
+    bufferUse = str2double(A{1});
+    if bufferParam ~= bufferUse
+        setParam('secondaryFiles:buffer',bufferUse);
+    end
+    
+    
     for i = 1:length(K)
         naked = fileData(K(i),'NakedName');
         ext   = fileData(K(i),'Extension');
@@ -11,7 +24,7 @@ function createSecondaryFile( K )
         if name
             Fs = fileData(K(i),'Fs');
             fullpath = strcat( path, name );
-            TS = createSecondaryTS( K(i) );
+            TS = createSecondaryTS( K(i), bufferUse/1000 );
             audiowrite( fullpath, TS, Fs );
             addDataFromFile( path, name );
         end
