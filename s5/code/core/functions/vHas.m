4aa0264d6fe62d7ef4bda360b2694b2acd35b9ba
uido123 <uido123@gmail.com>
2015-10-21 17:03:22 +0300

    call analysis window

--- s5/code/core/functions/vHas.m
+++ s5/code/core/functions/vHas.m
@@ -1,11 +1,17 @@
-function [ has, idx ] = vHas( V,str )
+function [ has, idx, next ] = vHas( V,str )
 %VHAS check varargin has a parameter name. Returns "has" and index
 
     idx = find(strcmp(V,str),1);
     if isempty(idx)
         has = false;
+        next = [];
     else
         has = true;
+        try
+            next = V{idx+1};
+        catch 
+            next = [];
+        end
     end
 
 end
